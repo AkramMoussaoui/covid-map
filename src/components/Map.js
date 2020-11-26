4706dc26d86f489acf0bccb7aa8b1b41621fda96
Akram <akrammoussaoui14@gmail.com>
2020-11-26 19:31:13 +0100

    finished

---
+++ src/components/Map.js
@@ -0,0 +1,90 @@
+import ReactMapGL from "react-map-gl";
+import { useState, useEffect } from "react";
+import { Marker, Popup } from "react-map-gl";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const Map = () => {
+  const history = useHistory();
+  const [countries, setCountries] = useState();
+  const [pop, setPop] = useState({ show: false });
+  const [viewport, setViewport] = useState({
+    width: "fit",
+    height: "100vh",
+    latitude: 24.97393057661664,
+    longitude: 2.156883421379434,
+    zoom: 3.909629491061953,
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(
+        "https://disease.sh/v3/covid-19/countries"
+      );
+      console.log(data);
+      setCountries(data);
+    };
+    getData();
+  }, []);
+
+  const handleClick = (country) => {
+    history.push(`/${country}`);
+  };
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={(nextViewport) => {
+        setViewport(nextViewport);
+      }}
+      mapboxApiAccessToken={process.env.REACT_APP_TOKEN}
+      mapStyle="mapbox://styles/akram25/cjyummsdt092r1cs4fsivp0tu"
+    >
+      {countries &&
+        countries.map((country) => {
+          return (
+            <Marker
+              key={country.country}
+              offsetTop={-48}
+              offsetLeft={-24}
+              latitude={country.countryInfo.lat}
+              longitude={country.countryInfo.long}
+            >
+              <img
+                style={{ width: "30px", height: "30px" }}
+                src={country.countryInfo.flag}
+                onMouseOver={() => setPop({ ...country, show: true })}
+                onMouseEnter={() => setPop({ ...country, show: true })}
+                onMouseMove={() => setPop({ ...country, show: true })}
+                onMouseLeave={() => setPop({ show: false })}
+                onClick={() => handleClick(country.country)}
+                alt="country"
+              />
+            </Marker>
+          );
+        })}
+      {pop.show && (
+        <Popup
+          className={`${pop.todayCases < 1000 ? "div2" : "div1"}`}
+          offsetTop={-60}
+          offsetLeft={-10}
+          tipSize={0}
+          dynamicPosition={false}
+          longitude={pop.countryInfo.long}
+          latitude={pop.countryInfo.lat}
+          closeButton={false}
+        >
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <h1>{pop.country}</h1>
+            <p>Today cases : {pop.todayCases}</p>
+          </div>
+        </Popup>
+      )}
+    </ReactMapGL>
+  );
+};
+
+export default Map;
---
+++ src/components/Stats.js
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import { VictoryChart, VictoryLine } from "victory";
+
+const Wrapper = styled.div`
+  background: #393e46;
+  overflow: hidden;
+  color: #ffd369;
+  box-sizing: border-box;
+  .container {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 2rem;
+  }
+
+  .buttons {
+    margin-top: 3rem;
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  button {
+    border-radius: 3px;
+    padding: 0.5rem 0;
+    width: 11rem;
+    background: transparent;
+    color: white;
+    border: 2px solid #000000;
+    font-size: 1.5rem;
+  }
+
+  button:hover {
+    background-color: #ffd369;
+    color: #393e46;
+    border-color: #ffd369;
+  }
+
+  button:focus {
+    background-color: #ffd369;
+    color: #393e46;
+    border-color: #ffd369;
+  }
+  h1 {
+    font-size: 3rem;
+  }
+
+  * {
+    margin: 0;
+  }
+`;
+
+const Stats = () => {
+  const { country } = useParams();
+  const [data, setData] = useState();
+  const [display, setDisplay] = useState();
+  const handleDisplay = (info) => {
+    setDisplay(data[info]);
+  };
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(
+        `https://disease.sh/v3/covid-19/historical/${country}?lastdays=30`
+      );
+      setData(data.timeline);
+      setDisplay(data.timeline.cases);
+    };
+    getData();
+  }, [country]);
+
+  return (
+    <Wrapper>
+      <div className="container">
+        <h1>{country}</h1>
+        <div className="buttons">
+          <button onClick={() => handleDisplay("cases")}>Cases</button>
+          <button onClick={() => handleDisplay("deaths")}>Deaths</button>
+          <button onClick={() => handleDisplay("recovered")}>Recovered</button>
+        </div>
+        {display && (
+          <VictoryChart
+            height={200}
+            padding={{
+              left: 80,
+              top: 20,
+              bottom: 80,
+              right: 20,
+            }}
+            style={{
+              parent: {
+                height: "50%",
+              },
+            }}
+            scale={{ x: "time" }}
+            animate={{
+              duration: 2000,
+              onLoad: { duration: 1000 },
+            }}
+          >
+            <VictoryLine
+              interpolation="linear"
+              data={Object.keys(display).map((data, index) => {
+                return {
+                  x: new Date(data),
+                  y: +Object.values(display)[index],
+                };
+              })}
+              style={{ data: { stroke: "#c43a31", strokeWidth: 1 } }}
+            />
+          </VictoryChart>
+        )}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Stats;
